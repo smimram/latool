29cfd2ef80be308f28d424155ab485bd95a8ec1f
Samuel Mimram <smimram@gmail.com>
2024-11-22 15:47:31 +0100

    Start removing comments.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 A tool to preprocess LaTeX files. The following options are currently supported:
 
 - `--expand`: replace `\input` commands by their content
+- `--remove-comments`: remove comments
 
 This is programmed in OCaml, improvements and suggestions are welcome.
--- src/latool.ml
+++ src/latool.ml
@@ -4,12 +4,14 @@ let () =
   let outfile = ref None in
   let fnames = ref [] in
   let expand = ref false in
+  let remove_comments = ref false in
   Arg.parse
     (Arg.align
        [
          ("--expand", Arg.Set expand, " Replace \\input by their content.");
          ("-o", Arg.String (fun s -> outfile := Some s), " Output file.");
          ("--output", Arg.String (fun s -> outfile := Some s), " Output file.");
+         ("--remove-comments", Arg.Set remove_comments, " Remove comments.");
        ]
     )
     (fun s -> fnames := s :: !fnames)
@@ -19,21 +21,30 @@ let () =
     | Some fname -> open_out fname
     | None -> stdout
   in
-  let process =
+  let rec process =
+    let re_comment = Str.regexp "%[^\n]\n" in
     let re_input = Str.regexp "\\\\input{\\([^}]*\\)}" in
     fun fname ->
-      File.contents fname |>
-      Str.global_substitute re_input (fun s ->
-          let i = Str.matched_group 1 s in
-          let i = if not (Sys.file_exists i) then i ^ ".tex" else i in
-          if not (Sys.file_exists i) then
-            (
-              Printf.eprintf "Could not find file: %s\n%!" i;
-              "NOT_FOUND"
-            )
-          else
-            File.contents i
-        )
+      let s = File.contents fname in
+      let s =
+        if not !remove_comments then s
+        else Str.global_replace re_comment "\n" s
+      in
+      let s =
+        if not !expand then s else
+          Str.global_substitute re_input (fun s ->
+              let i = Str.matched_group 1 s in
+              let i = if not (Sys.file_exists i) then i ^ ".tex" else i in
+              if not (Sys.file_exists i) then
+                (
+                  Printf.eprintf "Could not find file: %s\n%!" i;
+                  "NOT_FOUND"
+                )
+              else
+                process i
+            ) s
+      in
+      s
   in
   if !expand then
     List.iter (fun fname -> process fname |> output_string oc) !fnames

8e8c1e13c4fa81b7fc6a2bc9f1372f9e9f6bd1c0
Samuel Mimram <smimram@gmail.com>
2024-11-26 11:49:50 +0100

    Remaining time.

--- src/grammar.ml
+++ src/grammar.ml
@@ -3,6 +3,7 @@ open Common
 (** Check the contents of a LaTeX file and return corrections in markdown format. *)
 let check ?(stdout=true) ?(limit=20) s =
   let l = String.split_on_char '\n' s |> ref in
+  let t0 = Sys.time () in
   let lines = List.length !l in
   let ans = ref "" in
   let buf = ref "" in
@@ -16,7 +17,12 @@ let check ?(stdout=true) ?(limit=20) s =
     let buf = !buf in
     let buf = Printf.sprintf "```\n%s\n```\n" buf in
     if stdout then print_endline buf;
-    if !Settings.show_progress then Printf.printf "progress: %d%%\r%!" (100 - List.length !l * 100 / lines);
+    if !Settings.show_progress then
+      let l = List.length !l in
+      let dt = Sys.time () -. t0 in
+      Printf.printf "progress: %d%% (%d minutes remaining)\r%!"
+        (100 - l * 100 / lines)
+        (dt /. float (lines - l) *. float l /. 60. |> int_of_float);
     let corrections = Llama.spellcheck buf in
     if stdout then Printf.printf "%s\n\n%!" corrections;
     let chunk = Printf.sprintf "%s\n%s\n\n" buf corrections in
